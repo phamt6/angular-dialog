c914e247df4ea3f35f7ac28e048ab43a01fd70b7
Tien Hoang Pham <tienhoang.pham@gmail.com>
2021-01-24 20:13:57 +0200

    Init proj - setup folder structure

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.less']
+})
+export class DialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
